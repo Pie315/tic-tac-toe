bb457888d627a9cb486534fd72f8e0dc3334062b
Pie315 <devonkaelberer@gmail.com>
2023-03-24 16:58:06 -0400

    started planning out

---
+++ javascript.js
@@ -0,0 +1,14 @@
+console.log("ran");
+
+// game (Module?)
+// runs game determines a win/loss
+
+
+
+// player (factory)
+// Stoes wins/losses?
+
+
+
+// gameboard (Module)
+// stores position of pieces

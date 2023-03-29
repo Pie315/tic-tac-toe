a53ec38187000296b42b1294e54641e73444daee
Pie315 <devonkaelberer@gmail.com>
2023-03-28 21:33:50 -0400

    Module pattern demo

--- javascript.js
+++ javascript.js
@@ -3,12 +3,23 @@ console.log("ran");
 // game (Module?)
 // runs game determines a win/loss
 
-
-
 // player (factory)
 // Stoes wins/losses?
 
-
-
 // gameboard (Module)
 // stores position of pieces
+let boardModule = (function () {
+    const hello = "hello world";
+    let times = 0;
+
+    const counter = () => {
+        console.log("This has been run", times, "times."); // this is printed to the console
+        times += 1;
+        return "hello"; // this is returned as a value
+    };
+
+    return {
+        hello,
+        counter,
+    };
+})();
